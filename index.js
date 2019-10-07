bf5fdcdd9764a3d6b8f7a473a52cfa5a871182ab
Simone <simone.masoni@gmail.com>
2019-10-07 13:51:18 +0200

    Restarted from the basis

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express    = require('express');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app        = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 5000;
+
+let connDb = {};
+
+
+var router = express.Router(); 
+router.get('/song', function(req, res) {
+  
+});
+
+router.get('/song/:songId', function(req, res) {
+  var songId = req.params.songId;
+
+});
+
+
+app.use('/1', router);
+
+app.listen(port);
+console.log('Server listening on port: ' + port);
