eeb43b86c1b39e9e606d427314f76bfd84753ffe
Simone <simone.masoni@gmail.com>
2020-07-14 19:50:19 +0200

    V.10

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express    = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
-var app        = express();
-var bodyParser = require('body-parser');
+var app = express();
+var bodyParser = require("body-parser");
 
-var database = require('./database');
+var database = require("./database");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,28 +12,38 @@ app.use(cors());
 
 var port = process.env.PORT || 5000;
 
-let SongModel = require('./models/song');
+let SongModel = require("./models/song");
 
-var router = express.Router(); 
-router.get('/song', (req, res) => {
-  SongModel.find({}, 'title author').sort({title: 1}).then(docs => res.json(docs));
+var router = express.Router();
+router.get("/song", (req, res) => {
+  SongModel.find({}, "title author")
+    .sort({ title: 1 })
+    .then((docs) => res.json(docs));
 });
 
-router.get('/song/:songId', (req, res) => {
+router.get("/song/:songId", (req, res) => {
   var songId = req.params.songId;
-  SongModel.findById(songId).then(doc => res.json(doc));
+  SongModel.findById(songId).then((doc) => res.json(doc));
 });
 
-router.put('/song/:songId', (req, res) => {
+router.put("/song/:songId", (req, res) => {
   var songId = req.params.songId;
-  console.log("Need to update song with id " + songId);
-  SongModel.findByIdAndUpdate(songId, {lyrics: req.body.lyrics}).then(() => res.json({}));
+  SongModel.findByIdAndUpdate(songId, { lyrics: req.body.lyrics }).then(() =>
+    res.json({})
+  );
 });
 
-app.use('/1', router);
+router.post("/song", (req, res) => {
+  SongModel.create({
+    title: req.body.title,
+    author: req.body.author,
+    lyrics: req.body.lyrics,
+  }).then((doc) => res.json(doc));
+});
 
-app.use('/', express.static(__dirname + '/public/ui'));
+app.use("/1", router);
+
+app.use("/", express.static(__dirname + "/public/ui"));
 
 app.listen(port);
-console.log('Server listening on port: ' + port);
-
+console.log("Server listening on port: " + port);
